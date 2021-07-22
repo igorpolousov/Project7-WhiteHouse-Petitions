8badc035f2277c45e72f90f4d566aee8f13b297e
Igor Polousov <igorpolousov@gmail.com>
2021-07-22 12:03:02 +0300

    Initial Commit

---
+++ Project7 WhiteHouse Petitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project7 WhiteHouse Petitions
+//
+//  Created by Igor Polousov on 22.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
